965b1b70a1bbf60aee8f68c8e5301e2b82f55145
Mark Corrales <corrales.mark@epa.gov>
2022-04-26 01:59:33 -0400

    updated blockgroupstats to EJScreen2.0 but no PR

--- R/blockgroupstats.R
+++ R/blockgroupstats.R
@@ -3,22 +3,23 @@
 #' @title EJSCREEN demographic and enviromental indicators for Census block groups
 #'
 #' @description 
-#' This is essentially the EJSCREEN dataset, but 
-#'   could be replaced with something using different indicators.
+#' This is essentially the EJSCREEN dataset, plus more demographic subgroups.  
 #'   It has demographic and environmental data from EJSCREEN.
 #'   
 #' @details 
+#'   As of 4/2022 it is the EJScreen 2.0 version of data, which used ACS 2015-2019.
+#'   EJScreen 2.0 was released 2/18/2022 (raw data download avail 2/22/2022).
+#'   
 #'   NOTE: It also has the race/ethnic subgroups that add up to minority or people of color, 
 #'   while EJSCREEN as of Feb. 2022 version 2.0 did not have those variables for standard reports.
-#'   However those variables are not filled in for Puerto Rico, so 
-#'   US totals will include PR for pop but not for hisp, for example.
 #'   
-#'   EJSCREEN 2.0 was released 2/18/2022 (raw data download avail 2/22/2022).
+#'   **** PUERTO RICO IS NOT INCLUDED IN THE VERSION HERE AS OF LATE APRIL 2022.
 #'   
-#'   The 2020 version of EJSCREEN was released in 2020/2021 and used through Jan 2022. 
-#'   It is based on American Community Survey (ACS) data for 2014-2018.
+#'   Those demographic variables were not yet calculated for Puerto Rico, so 
+#'   US totals would include PR for pop but not for hisp, for example.
+#'   
 #'   Each year this could be created as for the latest version.
-#'   It is also available in a similar form via the ejscreen package on github, ejscreen::bg20 for the 2020 version, 
+#'   It is also available in a similar form via the ejscreen package on github, 
 #'   but there are differences in which columns are kept.
 #'   
 #'   It is a data.table of US Census blockgroups (not blocks). Approx 220,333 rows, approx 100 columns.
@@ -26,7 +27,7 @@
 #'   
 #'   column names include bgfips, pop, pctlowinc, pcthisp, etc.
 #'   
-#'   See notes on cleaning up and changing the dataset starting from ejscreen::bg20 or bg21
+#'   See notes on cleaning up and changing the dataset starting from ejscreen::bg21 or bg21plus
 #'   
 #'   see source code in EJAM::create_blockgroupstats
 #'   
